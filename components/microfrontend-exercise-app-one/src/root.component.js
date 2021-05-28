e829c7bbfa4fcd3f8810fe3217a48cb7ed53e5aa
Angel Rodriguez - vn51zwv <Angel.Rodriguez@walmart.com>
2021-05-27 23:52:12 -0500

    Added a fetch example

--- components/microfrontend-exercise-app-one/src/root.component.js
+++ components/microfrontend-exercise-app-one/src/root.component.js
@@ -3,19 +3,36 @@ import { BrowserRouter, Link } from "react-router-dom";
 
 import "./root.component.css";
 
-export default function Root(props) {
-  
-  return (
+class Root extends React.Component {
+  componentDidMount() {
+    fetch(
+      `https://5413ec42-f060-403e-b676-dd7d7b0a0631.mock.pstmn.io/products?page=1`
+    )
+      .then((res) => res.json())
+      .then(
+        (res) => {
+          console.log(res);
+        },
+        (e) => {
+          console.error(e);
+        }
+      );
+  }
+
+  render() {
+    return (
       <BrowserRouter>
-      <nav className="nav">
-        <Link to="/page1" className="link">
-          Page 1
-        </Link>
-        <Link to="/page2" className="link">
-          Page 2
-        </Link>
-      </nav>
-    </BrowserRouter>  
-    
-  );
+        <nav className="nav">
+          <Link to="/page1" className="link">
+            Page 1
+          </Link>
+          <Link to="/page2" className="link">
+            Page 2
+          </Link>
+        </nav>
+      </BrowserRouter>
+    );
+  }
 }
+
+export default Root;
